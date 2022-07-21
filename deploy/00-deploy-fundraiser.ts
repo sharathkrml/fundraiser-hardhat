646bbea1db66f06a4d4a53c893dd21ebe8972a81
Sharath <sharathkrml@gmail.com>
2022-07-21 09:17:50 +0530

    added verify script

--- deploy/00-deploy-fundraiser.ts
+++ deploy/00-deploy-fundraiser.ts
@@ -1,16 +1,20 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { network } from "hardhat"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
 
 const deployFundraiser = async (hre: HardhatRuntimeEnvironment) => {
-  const { deployments, getNamedAccounts } = hre;
-  const { deploy, log } = deployments;
-  const { deployer } = await getNamedAccounts();
-  await deploy("Fundraiser", {
-    from: deployer,
-    args: [],
-    log: true,
-  });
-  log("-----------------------------------");
-};
-export default deployFundraiser;
+    const { deployments, getNamedAccounts } = hre
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    await deploy("Fundraiser", {
+        from: deployer,
+        args: [],
+        log: true,
+    })
+    if (["hardhat", "localhost"].includes(network.name)) {
+        console.log(network.name)
+    }
+    log("-----------------------------------")
+}
+export default deployFundraiser
 
-deployFundraiser.tags = ["all"];
+deployFundraiser.tags = ["all"]
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,13 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-deploy";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import "hardhat-deploy"
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
-  namedAccounts: {
-    deployer: 0,
-    user: 1,
-  },
-};
+    solidity: "0.8.9",
+    namedAccounts: {
+        deployer: 0,
+        user: 1,
+    },
+    defaultNetwork: "hardhat",
+}
 
-export default config;
+export default config
